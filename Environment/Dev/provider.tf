7ebebabe50269272d14c1072522ec9077c449de0
Ashisdas-sketch <ashisdas158@gmail.com>
2025-09-11 00:59:22 +0530

    backend set new

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.41.0"
     }
   }
   backend "azurerm" {
-    resource_group_name   = "RG111"
-    storage_account_name  = "infrastorage143"
-    container_name        = "infracontainer"
-    key                   = "ashisfile.tfstate"
-    
+    resource_group_name  = "RG111"
+    storage_account_name = "infrastorage143"
+    container_name       = "infracontainer"
+    key                  = "ashisfile.tfstate"
+
   }
 }
 
 provider "azurerm" {
-    features {}
-    subscription_id = "07181b5e-cb73-4072-8605-0c0f9f4ae28f"
+  features {}
+  subscription_id = "07181b5e-cb73-4072-8605-0c0f9f4ae28f"
 }
